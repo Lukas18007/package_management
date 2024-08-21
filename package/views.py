6d9c9bc5e90767d47c5e75f2a4f458ee14fbb2fb
Lukas_leandro <leandrolukas916@gmail.com>
2024-08-21 09:52:59 -0300

    display if package was authorized or not in response

--- package/views.py
+++ package/views.py
@@ -12,22 +12,26 @@ class UpdatePackageStatusView(APIView):
             return Response({"error": "Package not found."}, status=status.HTTP_404_NOT_FOUND)
 
         status_received = request.data.get("status")
+        authorization_status = None
 
         if status_received == "aprovado":
             package.authorized = True
             package.authorized_at = date.today()
+            authorization_status = 'Authorized'
 
         elif status_received == "aprovado com apontamentos":
             if package.risk == 'B' or package.risk == 'M':
                 package.authorized = True
                 package.authorized_at = date.today()
+                authorization_status = 'Authorized'
             else:
                 package.authorized = False
+                authorization_status = 'Not authorized'
 
         elif status_received == "reprovado":
             package.authorized = False
+            authorization_status = 'Not authorized'
 
         package.save()
 
-        return Response({"message": "Package status updated successfully."}, status=status.HTTP_200_OK)
-
+        return Response({"message": f"Package status updated successfully: {authorization_status}"}, status=status.HTTP_200_OK)
