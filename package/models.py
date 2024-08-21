bebbd17069528bfab3ed973638634004a809c90a
Lukas_leandro <leandrolukas916@gmail.com>
2024-08-21 09:02:06 -0300

    create model and migration for packages

--- package/models.py
+++ package/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Package(models.Model):
+    id = models.AutoField(primary_key=True)
+    description = models.TextField()
+    authorized = models.BooleanField(default=False)
+    authorized_at = models.DateField(default=None, null=True, blank=True)
+    risk = models.CharField(max_length=1)
