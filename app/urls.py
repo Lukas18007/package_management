05f9d79c95145d7ab8969d5a2269b0e8ac05c52e
Lukas_leandro <leandrolukas916@gmail.com>
2024-08-21 09:30:57 -0300

    rename path for authorize

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from package.views import UpdatePackageStatusView
 
 urlpatterns = [
-    path('package/<int:id>/update-status/', UpdatePackageStatusView.as_view(), name='update-package-status'),
+    path('package/<int:id>/authorize/', UpdatePackageStatusView.as_view(), name='update-package-status'),
 ]
